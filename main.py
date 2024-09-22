95bc68c01241ae3c1bdc9ccefff8afea95e671a7
Ivory Tang <ivoryttang@gmail.com>
2024-09-22 10:03:51 -0700

    simplify lora

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ api_key = os.environ["MISTRAL_API_KEY"]
 model = "mistral-large-latest"
 client = Mistral(api_key=api_key)
 
-# Flux generate outfits
+# Flux generate outfits 
 async def get_outfit(prompt: str):
     handler = fal_client.submit(
         "fal-ai/flux-lora",
@@ -26,6 +26,27 @@ async def get_outfit(prompt: str):
 
     result = handler.get()
     return result
+
+async def get_outfit_with_lora(prompt: str, lora: str):
+    handler = fal_client.submit(
+        "fal-ai/flux-lora",
+        arguments={
+            "prompt": prompt,
+            "loras": [
+                {
+                "path": lora
+                }
+            ],
+            "image_size": "landscape_4_3",
+            "num_inference_steps": 28,
+            "guidance_scale": 3.5,
+            "num_images": 1,
+            "enable_safety_checker": True,
+            "output_format": "jpeg"
+        }
+    )
+    result = handler.get()
+    return result['images'][0]
 
 # FAL Try-on API: https://fal.ai/models/fal-ai/cat-vton/api?platform=python
 async def try_on(human_image: Image, outfit_image: Image):
@@ -65,24 +86,9 @@ def get_outfit_descriptions(count: int, human_image_url: str):
             ]
         )
         outfit_description = response.choices[0].message.content
-        print(outfit_description)
+        print("outfit description",i)
         outfit_descriptions.append(outfit_description)
     return outfit_descriptions
-
-# New image/text to video API from Luma
-async def generate_show(image_url: str):
-    handler = fal_client.submit(
-        "fal-ai/luma-dream-machine",
-        arguments={
-            "prompt": "Low-angle shot of the person in the photo walking the runway surrounded by a captivated audience",
-            "image_url": image_url,
-            "aspect_ratio": "16:9"
-        },
-    )
-
-    result = handler.get()
-    print(result)
-    return result["video"]["url"]
 
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
@@ -135,102 +141,77 @@ if __name__ == "__main__":
             all_outfits_2 = get_outfit_descriptions(1, new_human_url_2)
 
             # Use async functions to get and try outfits
-            async def generate_outfits(human_url: str, outfits: list):
+            # empty string = no lora
+            async def generate_outfits(human_url: str, outfits: list, lora: str):
                 outfit_images = []
                 for i in range(len(outfits)):
                     # Get outfit image using Flux
-                    outfit_image_url = await get_outfit(outfits[i])
-                    print("OUTFIT IMAGE URL", human_url, outfit_image_url['images'][0]['url'])
-                    # Call the try-on function with the uploaded images
-                    outfit = await try_on(human_url, outfit_image_url['images'][0]['url'])
+                    if len(lora) > 0:
+                        outfit = await get_outfit_with_lora(outfits[i], lora)
+                    else:
+                        outfit_image_url = await get_outfit(outfits[i])
+                        print("OUTFIT IMAGE URL", human_url, outfit_image_url['images'][0]['url'])
+                        # Call the try-on function with the uploaded images
+                        outfit = await try_on(human_url, outfit_image_url['images'][0]['url'])
 
                     # add outfit
                     outfit_images.append(outfit['url'])
                     print("tried on successfully")
                 
                 # Display the generated outfit images in a grid
-                cols = st.columns(len(outfits))  
+                cols = st.columns(len(outfit_images))  
                 for i, img_url in enumerate(outfit_images):
                     with cols[i]:  # Use the corresponding column for each image
-                        if st.image(img_url, caption=f"Outfit #{i + 1}"):
-                            selected_image = img_url  # Set the selected index when the image is clicked
-                        if selected_image is not None:
-                            st.success(f"You selected Outfit #{i + 1}")
+                        st.image(img_url, caption=f"Outfit #{i + 1}")
                         print("Showing outfits here")
-                return outfit_images, selected_image
-            
-            async def try_on_with_lora(outfits: list, lora: str):
-                outfit_images = []
-                for i in range(len(outfits)):
-                    handler = fal_client.submit(
-                        "fal-ai/flux-lora",
-                        arguments={
-                            "prompt": outfits[i],
-                            "loras": [
-                                {
-                                "path": lora
-                                }
-                            ],
-                            "image_size": "landscape_4_3",
-                            "num_inference_steps": 28,
-                            "guidance_scale": 3.5,
-                            "num_images": 1,
-                            "enable_safety_checker": True,
-                            "output_format": "jpeg"
-                        }
-                    )
-                    result = handler.get()
-
-                    # add outfit
-                    outfit_images.append(result['images'][0]['url'])
-                    print("tried on successfully")
-
-                    # Display the generated outfit images in a grid
-                    cols = st.columns(len(outfit_images))  
-                    for i, img_url in enumerate(outfit_images):
-                        with cols[i]:  # Use the corresponding column for each image
-                            if st.image(img_url, caption=f"Outfit #{i + 1}"):
-                                selected_image = img_url  # Set the selected index when the image is clicked
-                    if selected_image is not None:
-                        st.success(f"You selected Outfit #{i + 1}")
-                    return outfit_images, selected_image
+                return outfit_images, outfit_images[0]
                 
             
             if lora and lora2:
-                outfit, selected_image = asyncio.run(try_on_with_lora(all_outfits, lora))
-                outfit_2, selected_image_2 = asyncio.run(try_on_with_lora(all_outfits_2, lora2))
+                outfit, selected_image = asyncio.run(generate_outfits(new_human_url, all_outfits, lora))
+                outfit_2, selected_image_2 = asyncio.run(generate_outfits(new_human_url_2, all_outfits_2, lora2))
             else:
-                outfit, selected_image = asyncio.run(generate_outfits(new_human_url, all_outfits))
-                outfit_2, selected_image_2 = asyncio.run(generate_outfits(new_human_url_2, all_outfits_2))
+                outfit, selected_image = asyncio.run(generate_outfits(new_human_url, all_outfits, False))
+                outfit_2, selected_image_2 = asyncio.run(generate_outfits(new_human_url_2, all_outfits_2, False))
 
             st.success("Try-on process completed. Check the console for the result.")
 
         
             with col2:
+                print(selected_image)
                 if st.button("Generate Fashion Show"):
-                    print("Show is generating")
-                    # use new Luma AI API to generate fashion show given image that is clicked on
-                    video_placeholder_1 = st.empty()
-                    video_placeholder_2 = st.empty()
+                    # New image/text to video API from Luma
+                    async def generate_show(image_url: str):
+                        handler = fal_client.submit(
+                            "fal-ai/luma-dream-machine",
+                            arguments={
+                                "prompt": "Full body shot of the person in the photo walking the runway surrounded by a captivated audience",
+                                "image_url": image_url,
+                                "aspect_ratio": "16:9"
+                            },
+                        )
 
-                    # Display a loading message
-                    with st.spinner("Generating and loading videos..."):
-                        # Generate and display the first video
+                        result = handler.get()
+                        print(result)
+                        return result["video"]["url"]
+                    # use new Luma AI API to generate fashion show given image that is clicked on
+                    try:
                         video_url = asyncio.run(generate_show(selected_image))
+                        st.write(f"Video URL: {video_url}")
+
                         response = requests.get(video_url)
                         if response.status_code == 200:
                             video_bytes = response.content
-                            video_placeholder_1.video(video_bytes)
+                            st.video(video_bytes)
                         else:
-                            video_placeholder_1.error("Failed to retrieve the first video.")
-
-                        # Generate and display the second video
-                        video_url_2 = asyncio.run(generate_show(selected_image_2))
-                        response_2 = requests.get(video_url_2)
-                        if response_2.status_code == 200:
-                            video_bytes_2 = response_2.content
-                            video_placeholder_2.video(video_bytes_2)
-                        else:
-                            video_placeholder_2.error("Failed to retrieve the second video.")
-
+                            st.error("Failed to retrieve video.")
+                    except Exception as e:
+                        st.error(f"Error generating video: {e}")
+                    # video_url_2 = asyncio.run(generate_show(selected_image_2))
+                    # response_2 = requests.get(video_url_2)
+                    # if response_2.status_code == 200:
+                    #     video_bytes = response.content
+                    #     st.video(video_bytes)
+                    # else:
+                    #     st.error("Failed to retrieve video.")
                     st.text("Finished show")
